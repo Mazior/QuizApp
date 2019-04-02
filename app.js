436a04374383755a42d0c1c953a47b47fafdd317
Mazior <jzioria@gmail.com>
2019-04-02 08:46:54 -0800

    adding a few more questions

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function showProgress(){
 
 function showScores() {
 	var gameOverHtml = "<h1>Result</h1>";
-	gameOverHtml += "<h2 id = 'score' > Your scores :" + quiz.score + " </h2>";
+	gameOverHtml += "<h2 id = 'score' > Your scores : " + quiz.score + " </h2>";
 	var element = document.getElementById("quiz");
 	element.innerHTML = gameOverHtml;
 	
@@ -54,7 +54,11 @@ var questions  =[
     new Question("What is the current currency in Ghana called?",["Yen", "Cedi","kudi","sikaPa"],"Cedi"),
     new Question("What is the largest tribe in Ghana",["Ewe", "Akan","Guan","Ga"], "Akan"),
     new Question("Before colonization, Ghanaian's traded using",["Barter System", "Pound Sterling","Cedi","Kakai"], "Barter System","Barter System"),
-    new Question("Which lake in Ghana is the one of the largest man-made lakes in the world?",[" Lake Bosumtwi", "Lake Chad","Lake Volta","Lake Kainji"], "Lake Volta")
+    new Question("Which lake in Ghana is the one of the largest man-made lakes in the world?",[" Lake Bosumtwi", "Lake Chad","Lake Volta","Lake Kainji"], "Lake Volta"),
+    new Question("This Ghanaian leader was considered to be the first-ever president of the Republic of Ghana, and an extremely significant figure in its fight for independence from the British. Who was he?",[" Joseph Arthur Ankrah", "Kwame Nkrumah"," John Kufour"," Fred Akuffo"],
+	"Kwame Nkrumah"),
+	new Question( "which year did Ghana gain her independence?", ["6th March 1956", "6th March 1950","6th March 1957","6th March 1954"],"6th March 1957")
+
 ];
 
 var quiz = new Quiz(questions);
